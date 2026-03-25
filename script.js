0ad311aae7bd4c5b72de3f599caef3c5cc126b43
Karenbonisch <Karenbonisch@gmail.com>
2026-03-25 11:16:07 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function boxclick(event) {
-    let box = event.currentTarget // variavel
-    let text = box.querySelector("div") //suche div in der box
-    text.classList.toggle("versteck") //esconde se clicar e mostra se clicar novamente
-    let textweg =box.querySelector(".weg")
-    textweg.classList.toggle("versteck")
-    box.classList.toggle("gross") /* tamanho das caixas kind*/
-}
-/*qdo clicar na caixa div kind executa funcao para mostrar texto e esconder texto*/
